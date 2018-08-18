520bbf7754b4d2cd0d0eb3a6799b80b92ee94f6a
Vitalya <yaroshukvi@gmail.com>
2018-08-18 20:24:26 +0300

    Initial commit

---
+++ src/by/yaroshuk/Main.java
@@ -0,0 +1,13 @@
+package by.yaroshuk;
+
+public class Main {
+
+    public static void main(String[] args) {
+     int [] d = {1, 4, 6, 7, 4};
+        for (int i = 0; i < d.length ; i++) {
+          System.out.println(i + ") " + d[i]);
+        }
+
+
+    }
+}
